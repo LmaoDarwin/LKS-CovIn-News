ea74643bb48979bffa5fd3c9cc899b7cc1dafab7
darwinLin <d.f.winzi2006@gmail.com>
2023-01-29 16:09:56 +0700

    news 2 completion and autoscroll improvements

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 const nav = document.getElementById("navbar");
-const mainBg = document.getElementById("mainBg");
 const snap = document.getElementsByClassName("snap");
+const category = document.getElementById("category");
+const snapId = document.getElementById("snap");
 
 //nav js
 document.addEventListener("scroll", () => {
@@ -11,18 +12,30 @@ document.addEventListener("scroll", () => {
     nav.classList.add("scrolled");
   }
 });
+category.addEventListener("click", (ev) => console.log(ev));
 //end nav js
 
 //Snap scroll
-const basePx = 940
-let childOfSnap = snap.item(0).children.length - 3 //<< 3 for button excluded + offset basePx
-const snapPx = basePx * childOfSnap
+//init variables:
+let childOfSnap = snap.item(0).children.length - 3; //<< 3 for button excluded + offset basePx
 let coord = 0;
 let autoScroll;
+const basePx = 990;
+const snapPx = basePx * childOfSnap;
+//onhover autostop:
+snapId.addEventListener("mouseenter", () => clearInterval(autoScroll));
+snapId.addEventListener("mouseleave", () => clearAutoScroll());
+//stopper:
+function clearAutoScroll() {
+  clearInterval(autoScroll);
+  autoScroll = null;
+  setTimeout(() => getAutoScroll());
+}
+//starter:
 function getAutoScroll() {
   if (!autoScroll) {
     return (autoScroll = setInterval(() => {
-      console.log({coord,childOfSnap,snapPx,autoScroll,num:(940*4)});
+      // console.log({coord,childOfSnap,snapPx,autoScroll,num:(940*4)});
       if (coord >= snapPx) {
         coord = -basePx;
       }
@@ -30,17 +43,12 @@ function getAutoScroll() {
     }, 3000));
   }
 }
+//button onclick:
 function buttonScroll(nodeNumer, orien) {
   orien == "right" && snap.item(nodeNumer).scrollTo((coord += basePx), 0);
   orien == "left" && snap.item(nodeNumer).scrollTo((coord -= basePx), 0);
   if (coord <= 0) coord = 0;
-  if (coord >= snapPx) {
-    coord = snapPx;
-    return true;
-  }
-  clearInterval(autoScroll);
-  autoScroll = null;
-  setTimeout(() => getAutoScroll(), 10000);
+  if (coord >= snapPx) coord = snapPx;
 }
 getAutoScroll();
 //end snap scroll
