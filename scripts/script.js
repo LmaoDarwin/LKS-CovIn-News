c92bdeb14458ebb79baa759d830f63593d6cae3a
darwinLin <d.f.winzi2006@gmail.com>
2023-02-06 11:40:14 +0700

    commit responsive

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,7 @@
 const nav = document.getElementById("navbar");
-const snap = document.getElementsByClassName("snap");
 const category = document.getElementById("category");
-const snapId = document.getElementById("snap");
-
-//nav js
+let snapId = document.getElementById("snap");
+//<<< nav js >>>
 document.addEventListener("scroll", () => {
   if (this.scrollY == 0) {
     nav.classList.remove("scrolled");
@@ -12,43 +10,43 @@ document.addEventListener("scroll", () => {
     nav.classList.add("scrolled");
   }
 });
-category.addEventListener("click", (ev) => console.log(ev));
-//end nav js
+category.addEventListener("click", () => {
+  nav.children.item(2).classList.toggle("show");
+});
+//<<< end nav js >>>
 
-//Snap scroll
+//<<< Snap scroll >>>
 //init variables:
-let childOfSnap = snap.item(0).children.length - 3; //<< 3 for button excluded + offset basePx
+let BASEPX = snapId.clientWidth;
+let PX_TO_SCROLL = snapId.scrollWidth - snapId.clientWidth;
 let coord = 0;
 let autoScroll;
-const basePx = 990;
-const snapPx = basePx * childOfSnap;
 //onhover autostop:
-snapId.addEventListener("mouseenter", () => clearInterval(autoScroll));
-snapId.addEventListener("mouseleave", () => clearAutoScroll());
-//stopper:
-function clearAutoScroll() {
+snapId.parentNode.addEventListener("mouseenter", () =>
+  clearInterval(autoScroll)
+);
+snapId.parentNode.addEventListener("mouseleave", () => stopAutoScroll());
+function stopAutoScroll() {
   clearInterval(autoScroll);
   autoScroll = null;
-  setTimeout(() => getAutoScroll());
+  startAutoScroll();
 }
-//starter:
-function getAutoScroll() {
+function startAutoScroll() {
   if (!autoScroll) {
     return (autoScroll = setInterval(() => {
-      // console.log({coord,childOfSnap,snapPx,autoScroll,num:(940*4)});
-      if (coord >= snapPx) {
-        coord = -basePx;
-      }
-      snap.item(0).scrollTo((coord += basePx), 0);
+      if (coord >= PX_TO_SCROLL) coord = -BASEPX;
+      if (coord % snapId.clientWidth !== 0) coord = -BASEPX;
+      PX_TO_SCROLL = snapId.scrollWidth - snapId.clientWidth;
+      BASEPX = snapId.clientWidth;
+      snapId.scrollTo((coord += BASEPX), 0);
     }, 3000));
   }
 }
-//button onclick:
-function buttonScroll(nodeNumer, orien) {
-  orien == "right" && snap.item(nodeNumer).scrollTo((coord += basePx), 0);
-  orien == "left" && snap.item(nodeNumer).scrollTo((coord -= basePx), 0);
+function buttonScroll(orien) {
+  orien == "right" && snapId.scrollTo((coord += BASEPX), 0);
+  orien == "left" && snapId.scrollTo((coord -= BASEPX), 0);
   if (coord <= 0) coord = 0;
-  if (coord >= snapPx) coord = snapPx;
+  if (coord >= PX_TO_SCROLL) coord = -BASEPX;
 }
-getAutoScroll();
-//end snap scroll
+startAutoScroll();
+//<<< end snap scroll >>>
